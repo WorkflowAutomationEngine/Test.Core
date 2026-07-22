bc97c2a0ff05493ef7401413f737d6a8b3bfb77d
Vishal Astik <vishalastik@gmail.com>
2026-07-22 18:56:30 +0530

    Added Employee DOB field/property

--- Test.Core/Models/Employee.cs
+++ Test.Core/Models/Employee.cs
@@ -8,6 +8,8 @@ namespace Test.Core.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
+        public DateOnly? DateOfBirth { get; set; }
+
         public Employee()
         {
             Id = Guid.NewGuid();
