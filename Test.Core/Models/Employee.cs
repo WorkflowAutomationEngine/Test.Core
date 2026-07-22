0155bdf277c8d26b134bcdcf87fd2d7d36c2cfc4
Vishal Astik <vishalastik@gmail.com>
2026-07-22 19:26:09 +0530

    Added email field/property

--- Test.Core/Models/Employee.cs
+++ Test.Core/Models/Employee.cs
@@ -7,8 +7,8 @@ namespace Test.Core.Models
         public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
         public DateOnly? DateOfBirth { get; set; }
+        public string? Email{ get; set; }
 
         public Employee()
         {
